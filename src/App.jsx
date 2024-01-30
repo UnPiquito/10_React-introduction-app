cd5de256137fa8c9e8b143bd885d6164e81eb6ac
Unai Velo de los Santos <unai.velo@mataro.epiaedu.cat>
2024-01-30 18:44:03 +0100

    First_commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const name = "Unai";
+
+  return (
+    <>
+      <h1>Hello {name}</h1>
+    </>
+  )
+}
+
+export default App
